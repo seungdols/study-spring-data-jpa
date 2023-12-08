8e191fe75b5d69f98016a806cecaaca8bea3cd14
seungdols <seungdols0822@gmail.com>
2023-12-08 20:24:43 +0900

    Initial commit

---
+++ src/main/kotlin/com/seungdols/dev/datajpa/DataJpaApplication.kt
@@ -0,0 +1,11 @@
+package com.seungdols.dev.datajpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DataJpaApplication
+
+fun main(args: Array<String>) {
+	runApplication<DataJpaApplication>(*args)
+}
---
+++ src/test/kotlin/com/seungdols/dev/datajpa/DataJpaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.seungdols.dev.datajpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DataJpaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
