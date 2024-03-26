6f933b87bb209ef77de8d7cf93d8d8333d55c43a
seungdols <seungdols0822@gmail.com>
2024-03-26 19:38:19 +0900

    Auditing - Extract time entity

--- src/main/kotlin/com/seungdols/dev/datajpa/sample/domain/BaseTimeEntity.kt
+++ src/main/kotlin/com/seungdols/dev/datajpa/sample/domain/BaseTimeEntity.kt
@@ -3,17 +3,19 @@ package com.seungdols.dev.datajpa.sample.domain
 import jakarta.persistence.Column
 import jakarta.persistence.EntityListeners
 import jakarta.persistence.MappedSuperclass
-import org.springframework.data.annotation.CreatedBy
-import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
 
 @EntityListeners(AuditingEntityListener::class)
 @MappedSuperclass
-open class BaseEntity : BaseTimeEntity() {
-    @CreatedBy
+open class BaseTimeEntity {
+    @CreatedDate
     @Column(updatable = false)
-    var createdBy: String = ""
+    var createdDate: LocalDateTime = LocalDateTime.now()
 
-    @LastModifiedBy
-    var lastModifiedBy: String = ""
+    @LastModifiedDate
+    var lastModifiedDate: LocalDateTime = LocalDateTime.now()
 }
